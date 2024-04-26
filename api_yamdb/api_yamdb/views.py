f0342a1f10fd6a10fe3a862ca5898160a96dea07
Румянцев Савелий <radogask9rsd@gmail.com>
2024-04-26 08:46:20 +0500

    Fix urls

---
+++ api_yamdb/api_yamdb/views.py
@@ -0,0 +1,48 @@
+from rest_framework import viewsets
+
+from api_yamdb.models import Comment, Review
+
+from .permissions import AuthorOrReadOnly, ReadOnly
+from .serializers import (CommentSerializer, ReviewSerializer)
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    serializer_class = ReviewSerializer
+    permission_classes = (AuthorOrReadOnly,)
+    pagination_class = None
+
+    def get_permissions(self):
+        if self.action == 'retrieve':
+            return (ReadOnly(),)
+        return super().get_permissions()
+
+    def get_comment_id_kwarg(self):
+        return {'comment_id': self.kwargs['comment_id']}
+
+    def get_queryset(self):
+        return Review.objects.filter(**self.get_comment_id_kwarg())
+
+    def perform_create(self, serializer):
+        serializer.save(
+            author=self.request.user, **self.get_comment_id_kwarg())
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = (AuthorOrReadOnly,)
+    pagination_class = None
+
+    def get_permissions(self):
+        if self.action == 'retrieve':
+            return (ReadOnly(),)
+        return super().get_permissions()
+
+    def get_post_id_kwarg(self):
+        return {'post_id': self.kwargs['post_id']}
+
+    def get_queryset(self):
+        return Comment.objects.filter(**self.get_post_id_kwarg())
+
+    def perform_create(self, serializer):
+        serializer.save(
+            author=self.request.user, **self.get_post_id_kwarg())
