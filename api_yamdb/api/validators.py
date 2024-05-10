04373bdd4fc1d22c950f56d45636e85b829c3744
Савелий Румянцев <radogask9rsd@gmail.com>
2024-05-10 14:59:13 +0500

    datetime fix

--- api_yamdb/api/validators.py
+++ api_yamdb/api/validators.py
@@ -6,7 +6,7 @@ from django.core.exceptions import ValidationError
 def year_validator(year):
     if year < 0:
         raise ValidationError('Значение года не может быть отрицательным')
-    if year > datetime.datetime.now().year:
+    if year > datetime.now().year:
         raise ValidationError(
             ('Значение года не может быть больше текущего'),
         )
